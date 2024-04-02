e1a636a37f254e0bbc53f5c81d8400a336b2807c
fullsnacker <fullsnacker@gmail.com>
2024-04-02 12:07:56 -0300

    caso 0 resuelto

--- goncy-1/caso-0/src/App.tsx
+++ goncy-1/caso-0/src/App.tsx
@@ -1,28 +1,35 @@
-import type {Item} from "./types";
+import type { Item } from "./types";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./App.module.scss";
 import api from "./api";
 
 function App() {
-  const [items, setItems] = useState<Item[] | null>(null);
+  const [items, setItems] = useState<Item[]>([]);
 
   useEffect(() => {
     api.list().then(setItems);
   }, []);
 
+  const onHandleRemove = (id: number) => {
+    const newItems = items.filter((item) => item.id !== id);
+
+    setItems(newItems);
+  };
+
   return (
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form>
-        <input name="text" type="text" />
+        <input autoFocus name="text" type="text" />
         <button>Add</button>
       </form>
       <ul>
         {items.map((item) => (
-          <li className={item.completed ? styles.completed : ""}>
-            {item.text} <button>[X]</button>
+          <li key={item.id} className={item.completed ? styles.completed : ""}>
+            {item.text}
+            <button onClick={() => onHandleRemove(item.id)}>[X]</button>
           </li>
         ))}
       </ul>
